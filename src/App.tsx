935cc7cdbc477f73da2814f24e78097b44f016f0
david <1787068206@qq.com>
2022-05-14 20:29:02 +0800

    icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Aggregate, Blog, Codepen, Github, Image, Links, Run } from './icon';
 
 function App() {
   return (
@@ -10,6 +11,16 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          <Aggregate width="30" stroke="#096" />
+          <Blog width="30" stroke="#096" />
+          <Codepen width="30" fill="#096" />
+          <Github width="30" fill="#096" />
+          <Image width="30" stroke="#096" />
+          <Links width="30" stroke="#096" />
+          <Run width="30" stroke="#096" />
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
