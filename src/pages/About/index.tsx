bd454470d6083577d0e450ef46f24c572372f35c
david <1787068206@qq.com>
2022-06-19 21:46:37 +0800

    查看效果

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,34 +5,34 @@ import { githubQuery } from '../../utils/common'
 export default function About() {
   const { state } = useContext(Context)
   const [content, setContent] = useState<string>('')
-  const queryAbout = async () => {
-    // const res = await githubQuery({
-    //   url: "https://api.github.com/repos/huaasto/empty/issues/10",
-    //   method: "GET",
-    // })
-    const ql = `query {
-      repository(owner:"huaasto", name:"empty") {
-        issue(number: 10) {
-          bodyHTML
-          body
-          createdAt 
-          title
-        }
-      }
-    }`
-    const res = await githubQuery({
-      url: "https://api.github.com/graphql",
-      method: "POST",
-      data: { query: ql },
-      headers: state?.userInfo?.login !== "huaasto" ? {
-        Authorization: window.atob('dG9rZW4gZ2hwX04xdVV3TUlRamVvUERlZ2NUWkptbWVtSEh6bENVRDA1TmtjWQ==')
-      } : {}
-    })
-    setContent(res.data?.data?.repository?.issue?.bodyHTML)
-  }
-  useEffect(() => {
-    queryAbout()
-  }, [])
+  // const queryAbout = async () => {
+  //   // const res = await githubQuery({
+  //   //   url: "https://api.github.com/repos/huaasto/empty/issues/10",
+  //   //   method: "GET",
+  //   // })
+  //   const ql = `query {
+  //     repository(owner:"huaasto", name:"empty") {
+  //       issue(number: 10) {
+  //         bodyHTML
+  //         body
+  //         createdAt 
+  //         title
+  //       }
+  //     }
+  //   }`
+  //   const res = await githubQuery({
+  //     url: "https://api.github.com/graphql",
+  //     method: "POST",
+  //     data: { query: ql },
+  //     headers: state?.userInfo?.login !== "huaasto" ? {
+  //       Authorization: window.atob('dG9rZW4gZ2hwX04xdVV3TUlRamVvUERlZ2NUWkptbWVtSEh6bENVRDA1TmtjWQ==')
+  //     } : {}
+  //   })
+  //   setContent(res.data?.data?.repository?.issue?.bodyHTML)
+  // }
+  // useEffect(() => {
+  //   queryAbout()
+  // }, [])
   return (<>
     <div className='p-3 sm:p-10 bg-slate-400 h-full min-h-screen'>
       <div className='sm:fixed left-0 right-1/2 top-0 bottom-0 px-3 py-10 text-white whitespace-nowrap'>
