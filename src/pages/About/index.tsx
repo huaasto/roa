f2f23657f2d5dd99664c2a3954926bd04f8e6204
david <1787068206@qq.com>
2022-06-19 22:21:03 +0800

    阿萨大大

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -46,19 +46,18 @@ export default function About() {
 
 
   return (<div className="p-3 sm:p-10 bg-slate-400 h-full">
-    <div>3423</div>
     <div className="sm:fixed left-0 right-1/2 top-0 bottom-0 px-3 py-10 text-white whitespace-nowrap">
       <div className="flex justify-center items-center h-full">
         <span className="font-bold text-4xl">且听当世多变化</span>
       </div>
     </div>
-    {/* <div className="flex w-full flex-wrap sm:flex-nowrap">
+    <div className="flex w-full flex-wrap sm:flex-nowrap">
       <div className="flex-1 basis-80 w-1/2"></div>
       <div className=" flex-1 flex-shrink-0 basis-80">
-        <div className="bg-white p-4" dangerouslySetInnerHTML={{ __html: content.replaceAll("https://raw.githubusercontent.com/huaasto/blogPics/main", "https://cdn.jsdelivr.net/gh/huaasto/blogPics@master") || "" }}>
-        </div>
+        {/*  <div className="bg-white p-4" dangerouslySetInnerHTML={{ __html: content.replaceAll("https://raw.githubusercontent.com/huaasto/blogPics/main", "https://cdn.jsdelivr.net/gh/huaasto/blogPics@master") || "" }}>
+        </div>*/}
       </div>
-    </div> */}
+    </div>
   </div>)
 }
 
