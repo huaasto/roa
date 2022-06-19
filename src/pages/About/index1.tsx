29d18c18e5368c95ae9754f645d263098a9d6770
david <1787068206@qq.com>
2022-06-19 22:04:15 +0800

    反倒是

--- src/pages/About/index1.tsx
+++ src/pages/About/index1.tsx
@@ -6,10 +6,6 @@ export default function About() {
   const { state } = useContext(Context)
   const [content, setContent] = useState<string>('')
   const queryAbout = async () => {
-    // const res = await githubQuery({
-    //   url: "https://api.github.com/repos/huaasto/empty/issues/10",
-    //   method: "GET",
-    // })
     const ql = `query {
       repository(owner:"huaasto", name:"empty") {
         issue(number: 10) {
@@ -33,21 +29,18 @@ export default function About() {
   useEffect(() => {
     queryAbout()
   }, [])
-  return (<>
-    <div className='p-3 sm:p-10 bg-slate-400 h-full min-h-screen'>
-      <div className='sm:fixed left-0 right-1/2 top-0 bottom-0 px-3 py-10 text-white whitespace-nowrap'>
-        <div className='flex justify-center items-center h-full'>
-          <span className='font-bold text-4xl'>且听当世多变化</span>
-        </div>
+  return (<div className='p-3 sm:p-10 bg-slate-400 h-full min-h-screen'>
+    <div className='sm:fixed left-0 right-1/2 top-0 bottom-0 px-3 py-10 text-white whitespace-nowrap'>
+      <div className='flex justify-center items-center h-full'>
+        <span className='font-bold text-4xl'>且听当世多变化</span>
       </div>
-      <div className='flex w-full flex-wrap sm:flex-nowrap'>
-        <div className='flex-1 basis-80 w-1/2'></div>
-        <div className=' flex-1 flex-shrink-0 basis-80'>
-          <div className='bg-white p-4' dangerouslySetInnerHTML={{ __html: content.replaceAll("https://raw.githubusercontent.com/huaasto/blogPics/main", "https://cdn.jsdelivr.net/gh/huaasto/blogPics@master") || '' }}>
-          </div>
-        </div>
-      </div>
-
     </div>
-  </>)
+    <div className='flex w-full flex-wrap sm:flex-nowrap'>
+      <div className='flex-1 basis-80 w-1/2'></div>
+      <div className=' flex-1 flex-shrink-0 basis-80'>
+        <div className='bg-white p-4' dangerouslySetInnerHTML={{ __html: content.replaceAll("https://raw.githubusercontent.com/huaasto/blogPics/main", "https://cdn.jsdelivr.net/gh/huaasto/blogPics@master") || '' }}>
+        </div>
+      </div>
+    </div>
+  </div>)
 }
