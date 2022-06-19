692496782213f5ce87990b311cc585f387198f60
david <1787068206@qq.com>
2022-06-19 21:54:57 +0800

    ceshi

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,37 +5,38 @@ import { githubQuery } from '../../utils/common'
 export default function About() {
   const { state } = useContext(Context)
   const [content, setContent] = useState<string>('')
-  // const queryAbout = async () => {
-  //   // const res = await githubQuery({
-  //   //   url: "https://api.github.com/repos/huaasto/empty/issues/10",
-  //   //   method: "GET",
-  //   // })
-  //   const ql = `query {
-  //     repository(owner:"huaasto", name:"empty") {
-  //       issue(number: 10) {
-  //         bodyHTML
-  //         body
-  //         createdAt 
-  //         title
-  //       }
-  //     }
-  //   }`
-  //   const res = await githubQuery({
-  //     url: "https://api.github.com/graphql",
-  //     method: "POST",
-  //     data: { query: ql },
-  //     headers: state?.userInfo?.login !== "huaasto" ? {
-  //       Authorization: window.atob('dG9rZW4gZ2hwX04xdVV3TUlRamVvUERlZ2NUWkptbWVtSEh6bENVRDA1TmtjWQ==')
-  //     } : {}
-  //   })
-  //   setContent(res.data?.data?.repository?.issue?.bodyHTML)
-  // }
-  // useEffect(() => {
-  //   queryAbout()
-  // }, [])
+  const queryAbout = async () => {
+    // const res = await githubQuery({
+    //   url: "https://api.github.com/repos/huaasto/empty/issues/10",
+    //   method: "GET",
+    // })
+    const ql = `query {
+      repository(owner:"huaasto", name:"empty") {
+        issue(number: 10) {
+          bodyHTML
+          body
+          createdAt 
+          title
+        }
+      }
+    }`
+    const res = await githubQuery({
+      url: "https://api.github.com/graphql",
+      method: "POST",
+      data: { query: ql },
+      headers: state?.userInfo?.login !== "huaasto" ? {
+        Authorization: window.atob('dG9rZW4gZ2hwX04xdVV3TUlRamVvUERlZ2NUWkptbWVtSEh6bENVRDA1TmtjWQ==')
+      } : {}
+    })
+    setContent(res.data?.data?.repository?.issue?.bodyHTML)
+  }
+  useEffect(() => {
+    queryAbout()
+  }, [])
   return (<>
     <div className='p-3 sm:p-10 bg-slate-400 h-full min-h-screen'>
-      <div className='sm:fixed left-0 right-1/2 top-0 bottom-0 px-3 py-10 text-white whitespace-nowrap'>
+      324234 About
+      {/* <div className='sm:fixed left-0 right-1/2 top-0 bottom-0 px-3 py-10 text-white whitespace-nowrap'>
         <div className='flex justify-center items-center h-full'>
           <span className='font-bold text-4xl'>且听当世多变化</span>
         </div>
@@ -46,7 +47,7 @@ export default function About() {
           <div className='bg-white p-4' dangerouslySetInnerHTML={{ __html: content.replaceAll("https://raw.githubusercontent.com/huaasto/blogPics/main", "https://cdn.jsdelivr.net/gh/huaasto/blogPics@master") || '' }}>
           </div>
         </div>
-      </div>
+      </div> */}
 
     </div>
   </>)
