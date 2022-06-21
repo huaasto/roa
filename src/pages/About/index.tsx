20f4fa8e8ab80868de1ba0cc3b833747549c4395
david <1787068206@qq.com>
2022-06-21 22:36:35 +0800

    d

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -42,7 +42,7 @@ export default function About() {
     <div className="flex w-full flex-wrap sm:flex-nowrap">
       <div className="flex-1 basis-80 w-1/2"></div>
       <div className=" flex-1 flex-shrink-0 basis-80">
-        {content && <div className="bg-white p-4" dangerouslySetInnerHTML={{ __html: content.replaceAll("https://raw.githubusercontent.com/huaasto/blogPics/main", "https://cdn.jsdelivr.net/gh/huaasto/blogPics@master") || '' }}>
+        {content && <div className="bg-white p-4" dangerouslySetInnerHTML={{ __html: String(content)?.replaceAll("https://raw.githubusercontent.com/huaasto/blogPics/main", "https://cdn.jsdelivr.net/gh/huaasto/blogPics@master") || '' }}>
         </div>}
       </div>
     </div>
