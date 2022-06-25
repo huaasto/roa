88f350f32abffbfb647bef3e88b92e31e06c6061
david <1787068206@qq.com>
2022-06-25 10:18:27 +0800

    修复非github请求失败的bug

--- api/proxy.js
+++ api/proxy.js
@@ -26,11 +26,17 @@ const queryPostData = (req) => {
 
 module.exports = async (req, res) => {
   const postData = await queryPostData(req)
-  const orgSha = postData.sha.slice(0, 3) + postData.sha.slice(23) + postData.sha.slice(3, 23)
-  const realData = Buffer.from(orgSha, 'base64')
-  const reqData = Object.assign({}, JSON.parse(realData.toString()))
-  postData?.params && (reqData.params = postData.params)
-  postData?.data && (reqData.data = postData.data)
+  var reqData
+  if (postData.sha) {
+    const orgSha = postData.sha.slice(0, 3) + postData.sha.slice(23) + postData.sha.slice(3, 23)
+    const realData = Buffer.from(orgSha, 'base64')
+    reqData = Object.assign({}, JSON.parse(realData.toString()))
+    postData?.params && (reqData.params = postData.params)
+    postData?.data && (reqData.data = postData.data)
+  } else {
+    reqData = postData
+  }
+
   // console.log(postData.sha, postData.orgSha, orgSha === postData.orgSha)
   const data = await axios(reqData)
   if (!data.data) {
