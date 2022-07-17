5b74aa7c07c4b9e7d184ec6471851870cecb0113
david <1787068206@qq.com>
2022-07-17 18:50:11 +0800

    调整error报错显示

--- src/pages/Image/ImgWrap.tsx
+++ src/pages/Image/ImgWrap.tsx
@@ -14,7 +14,7 @@ export default function ImgWrap({ url, className, picClick, ...args }: TPicItem)
   }
   return (
     <div className={"inline-block h-36 w-18 m-2 relative bg-gray-600" + (className || '')} {...args}>
-      <img className="h-full w-full shadow-black object-cover" src={realUrl} alt="" onError={() => showError} onClick={picClick} />
+      <img className="h-full w-full shadow-black object-cover" src={realUrl} alt="" onError={showError} onClick={picClick} />
     </div>
   )
 }
